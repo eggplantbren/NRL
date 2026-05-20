286b0059750f5b6faec9012a0ddef6ded6aef9c3
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-20 14:50:51 +1200

    Neutral venue command line argument

--- predict.py
+++ predict.py
@@ -6,13 +6,18 @@ import sys
 posterior_sample = np.loadtxt("posterior_sample.txt")
 
 if __name__ == "__main__":
-    teams = [int(i) for i in sys.argv[1:]]
+
+    neutral = "--neutral" in sys.argv
+    teams = [int(i) for i in sys.argv[-2:]]
     assert(len(teams) == 2)
 
     abilities = posterior_sample[:, -17:]
     probs = np.empty(abilities.shape[0])
     for i in range(abilities.shape[0]):
         home_bonus = posterior_sample[i, 0]
+        if neutral:
+            home_bonus = 0.0
+
         loc = home_bonus + abilities[i, teams[0]] \
                 - abilities[i, teams[1]]
         shape = posterior_sample[i, 1]
