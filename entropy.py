bf6264b28c46afc21995bf640e5bb4a930292f08
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-31 16:58:09 +1200

    Histogram of favourite probability

--- entropy.py
+++ entropy.py
@@ -4,14 +4,20 @@
 
 import numpy as np
 from predict import prob
+import matplotlib.pyplot as plt
 
 posterior_sample = np.loadtxt("posterior_sample.txt")
 H = []
+pmax = []
 
 for i in range(17):
     for j in range(i):
         p = prob(i, j, posterior_sample, neutral=True)
+        pmax.append(np.max([p, 1.0-p]))
         H.append(-p*np.log(p) - (1.0 - p)*np.log(1.0 - p))
         print(i, j, flush=True)
 print(np.mean(H))
 
+plt.hist(pmax, 100)
+plt.xlabel("Winning Probability of Favourite")
+plt.show()
