2505d2fefa94d7e781bd3dff53675e57a9321338
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-31 16:45:49 +1200

    Refactored probability into a function

--- predict.py
+++ predict.py
@@ -2,7 +2,23 @@ import numpy as np
 from scipy.stats import t
 import sys
 
-posterior_sample = np.loadtxt("posterior_sample.txt")
+
+def prob(i, j, posterior_sample, neutral=False):
+    abilities = posterior_sample[:, -17:]
+    probs = np.empty(abilities.shape[0])
+    for i in range(abilities.shape[0]):
+        home_bonus = posterior_sample[i, 0]
+        if neutral:
+            home_bonus = 0.0
+
+        loc = home_bonus + abilities[i, teams[0]] \
+                - abilities[i, teams[1]]
+        shape = posterior_sample[i, 1]
+        scale = posterior_sample[i, 2]
+        probs[i] = 1.0 - t.cdf(0.0, df=shape, loc=loc, scale=scale)
+    return np.mean(probs)
+
+
 
 if __name__ == "__main__":
 
@@ -17,17 +33,6 @@ if __name__ == "__main__":
     neutral = "--neutral" in sys.argv
     assert(len(sys.argv) >= 3)
     teams = [lookup[name.lower()] for name in sys.argv[-2:]]
+    posterior_sample = np.loadtxt("posterior_sample.txt")
+    print(prob(teams[0], teams[1], posterior_sample, neutral=neutral))
 
-    abilities = posterior_sample[:, -17:]
-    probs = np.empty(abilities.shape[0])
-    for i in range(abilities.shape[0]):
-        home_bonus = posterior_sample[i, 0]
-        if neutral:
-            home_bonus = 0.0
-
-        loc = home_bonus + abilities[i, teams[0]] \
-                - abilities[i, teams[1]]
-        shape = posterior_sample[i, 1]
-        scale = posterior_sample[i, 2]
-        probs[i] = 1.0 - t.cdf(0.0, df=shape, loc=loc, scale=scale)
-    print(np.mean(probs))
